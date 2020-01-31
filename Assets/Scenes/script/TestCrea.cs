c2ed7e880238f7031bb5d7547cb8e696046dd419
jvittone <40358640+jvittone@users.noreply.github.com>
2020-01-31 09:46:19 +0100

    Add files via upload

---
+++ Assets/Scenes/script/TestCrea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCrea : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject prefab = (GameObject)Resources.Load("Graph");
+        GameObject graph = GameObject.Instantiate(prefab,new Vector3(-15,-12,53),Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
