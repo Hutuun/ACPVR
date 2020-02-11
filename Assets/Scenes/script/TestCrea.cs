292dab880e35b949bcebb652452e47c630ab5937
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-11 11:42:07 +0100

    Update TestCrea.cs

--- Assets/Scenes/script/TestCrea.cs
+++ Assets/Scenes/script/TestCrea.cs
@@ -26,7 +26,7 @@ public class TestCrea : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //creationCaracteristique();
+        creationCaracteristique();
 		creationSecteur();
     }
 	
