1126fd097e7041faac92bc06cb366f23693cceaf
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-26 10:12:39 +0100

    Update TestCrea.cs

--- Assets/Scenes/script/TestCrea.cs
+++ Assets/Scenes/script/TestCrea.cs
@@ -42,7 +42,7 @@ public class TestCrea : MonoBehaviour
 		
 		buttonReset.setTestCrea(this);
 		
-		buttonReset.setMoi(GetComponentInParent<realbutton>());
+		buttonReset.setMoi(GetComponentInParent<Cylinder>());
 		
 		//.GetComponent("SphereScript");
 		
