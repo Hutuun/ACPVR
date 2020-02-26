b2701349584a499bdbae356817721c2b41fe49fd
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-26 10:33:36 +0100

    Update SphereScript.cs

--- Assets/Scenes/script/SphereScript.cs
+++ Assets/Scenes/script/SphereScript.cs
@@ -35,7 +35,7 @@ public class SphereScript : MonoBehaviour
 	
 	private bool selectionne = false;
 	
-	private Material material = (Material)Material.Load("SphereMaterial");
+	private Material material = Resources.Load("SphereMaterial", typeof(Material)) as Material;
 
     // Start is called before the first frame update
     void Start()
