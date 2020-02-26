df7841d17eec33488d8dcfc8ea6598465189eaab
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-26 09:26:29 +0100

    Update SphereScript.cs

--- Assets/Scenes/script/SphereScript.cs
+++ Assets/Scenes/script/SphereScript.cs
@@ -377,6 +377,10 @@ public class SphereScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetMouseButtonDown(0))
+		{
+			Debug.Log("Pressed primary button.");
+			clique();
+		}
+	}
 }
