559507a5c71349e54c15727342a708de7d01cdc7
jvittone <40358640+jvittone@users.noreply.github.com>
2020-02-26 12:15:15 +0100

    Maj

--- Assets/Scenes/script/TestCrea.cs
+++ Assets/Scenes/script/TestCrea.cs
@@ -166,17 +166,11 @@ public class TestCrea : MonoBehaviour
 		
 		for (int i = 0; i < spheresSect.Count; i ++)
         {
-			spheresSect[i].transform.localPosition = new Vector3(centreGraphSect.x + (float)resSect2[i], centreGraphSect.y + (float)resSect2[i+1], centreGraphSect.z + (float)resSect2[i+2]);
-			
-			//spheresSect[i].transform.rotation = Quaternion.identity;
-				
-			//spheresSect[i].transform.Rotate(90,0,0,Space.Self);
+			spheresSect[i].transform.SetPositionAndRotation(new Vector3(centreGraphSect.x + (float)resSect2[i*3], centreGraphSect.y + (float)resSect2[i*3+1], centreGraphSect.z + (float)resSect2[i*3+2]),Quaternion.identity);
 			
 			SphereScript sphereScript = (SphereScript)spheresSect[i].GetComponent("SphereScript");
 			
 			sphereScript.rezoom(coeffSect);
-            
-			//Debug.Log(convert(resSect[i]));
         }
 	}
 	
@@ -196,17 +190,9 @@ public class TestCrea : MonoBehaviour
         {
 			spheresSect[i].transform.SetPositionAndRotation(new Vector3(centreGraphSect.x + (float)resSect2[i*3], centreGraphSect.y + (float)resSect2[i*3+1], centreGraphSect.z + (float)resSect2[i*3+2]),Quaternion.identity);
 			
-			//spheresSect[i].transform.localPosition = new Vector3(centreGraphSect.x + (float)resSect2[i], centreGraphSect.y + (float)resSect2[i+1], centreGraphSect.z + (float)resSect2[i+2]);
-			
-			//spheresSect[i].transform.rotation = Quaternion.identity;
-				
-			//spheresSect[i].transform.Rotate(90,0,0,Space.Self);
-			
 			SphereScript sphereScript = (SphereScript)spheresSect[i].GetComponent("SphereScript");
 			
 			sphereScript.rezoom(coeffSect);
-            
-			//Debug.Log(convert(resSect[i]));
         }
 	}
 	
@@ -227,12 +213,6 @@ public class TestCrea : MonoBehaviour
 			for (int i = 0; i < spheresSect.Count; i ++)
 			{
 				spheresSect[i].transform.SetPositionAndRotation(new Vector3(centreGraphSect.x + (float)resSect2[i*3], centreGraphSect.y + (float)resSect2[i*3+1], centreGraphSect.z + (float)resSect2[i*3+2]),Quaternion.identity);
-				
-				//spheresSect[i].transform.localPosition = new Vector3(centreGraphSect.x + (float)resSect2[i], centreGraphSect.y + (float)resSect2[i+1], centreGraphSect.z + (float)resSect2[i+2]);
-				
-				//spheresSect[i].transform.rotation = Quaternion.identity;
-				
-				//spheresSect[i].transform.Rotate(90,0,0,Space.Self);
 				
 				SphereScript sphereScript = (SphereScript)spheresSect[i].GetComponent("SphereScript");
 			
