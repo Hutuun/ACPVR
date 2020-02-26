01ea0d57804a2bbbb2f599b17c6829435eb82da4
Hutuun <sullivan1.honnet@gmail.com>
2020-02-26 15:00:59 +0000

    re

---
+++ Assets/Scenes/script/SuperScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperScript : MonoBehaviour
+{
+    public Vector3 origine = new Vector3(-1.57f, 0.7045f, 6.94f);
+
+    private GameObject gameObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject prefab = (GameObject)Resources.Load("ObjetPrincipal");
+        gameObject = GameObject.Instantiate(prefab, origine, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
